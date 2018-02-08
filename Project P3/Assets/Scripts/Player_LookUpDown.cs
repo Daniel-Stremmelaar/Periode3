21d810db751ef614baa632315875668c05a33a1b
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-02-08 15:44:29 +0100

    Player Controls
    Includes:
    -Movement front, back, left, right
    -Jumping
    -Look left, right, up, down

---
+++ Project P3/Assets/Scripts/Player_LookUpDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_LookUpDown : MonoBehaviour {
+    private Vector3 r;
+    public float rotSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+    }
+
+    private void Look()
+    {
+        r.x += -Input.GetAxis("Mouse Y");
+        r.x = Mathf.Clamp(r.x, -50.0f, 50.0f);
+        transform.eulerAngles = (new Vector3(r.x, transform.eulerAngles.y, 0.0f));
+    }
+}
---
+++ Project P3/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+    [Header("Movement")]
+    public float speed;
+    public float rotSpeed;
+
+    [Header("Jumping")]
+    public Vector3 velocity;
+    public int jumpCurrent;
+    public int jumpMax;
+
+    private bool mayJump;
+    private Rigidbody body;
+    private Vector3 v;
+    private Vector3 r;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Terrain")
+        {
+            jumpCurrent = 0;
+            mayJump = true;
+        }
+    }
+
+    //movement back-front & left-right
+    private void Move()
+    {
+        v.x = Input.GetAxis("Horizontal");
+        v.z = Input.GetAxis("Vertical");
+        transform.Translate(v * speed * Time.deltaTime);
+    }
+
+    //jumping
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (mayJump == true)
+            {
+                body.velocity = velocity;
+                jumpCurrent++;
+                if (jumpCurrent >= jumpMax)
+                {
+                    mayJump = false;
+                }
+            }
+        }
+    }
+
+    //look left and right
+    private void Look()
+    {
+        r.y = Input.GetAxis("Mouse X");
+        transform.Rotate(r * rotSpeed * Time.deltaTime);
+    }
+}
