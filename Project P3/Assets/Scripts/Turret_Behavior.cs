e455d622ea24e574b8792a6815c19bf07728e1f9
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-02-19 15:45:15 +0100

    Turret Rotation
    Partial fix for turret rotation

---
+++ Project P3/Assets/Scripts/Turret_Behavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Behavior : MonoBehaviour {
+
+    private GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.transform);
+        //clamp rotation between X = 80 and X = 133
+	}
+}
