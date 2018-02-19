c57deb7961d887b3653183cc38aeba1ee2950ede
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-02-19 13:59:43 +0100

    Test Object
    Add: reference train size

--- Project P3/Assets/Scripts/Player_LookUpDown.cs
+++ Project P3/Assets/Scripts/Player_LookUpDown.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player_LookUpDown : MonoBehaviour {
     private Vector3 r;
-    public float rotSpeed;
+    [SerializeField] private float rotSpeed;
 
     // Use this for initialization
     void Start()
@@ -20,7 +20,7 @@ public class Player_LookUpDown : MonoBehaviour {
 
     private void Look()
     {
-        r.x += -Input.GetAxis("Mouse Y");
+        r.x += -Input.GetAxis("Mouse Y") * rotSpeed;
         r.x = Mathf.Clamp(r.x, -50.0f, 50.0f);
         transform.eulerAngles = (new Vector3(r.x, transform.eulerAngles.y, 0.0f));
     }
