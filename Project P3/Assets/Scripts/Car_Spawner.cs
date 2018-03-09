77664025d582ae8abd2dc0c82b5bc18c87eb35c1
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-03-09 13:21:59 +0100

    spawner script en car script
    heb er een begin in gemaakt

--- Project P3/Assets/Scripts/Car_Spawner.cs
+++ Project P3/Assets/Scripts/Car_Spawner.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class Car_Spawner : MonoBehaviour
 {
     public GameObject car1;
+    public bool spawnCheck = true;
 
     private void Update()
     {
-        
+        MaySpawn();
+    }
+    public void MaySpawn()
+    {
+        if(spawnCheck == true)
+        {
+            StartCoroutine(SpawnTime());
+            Spawner();
+        }
     }
     public void Spawner()
     {
-        //for (int i = 0; i < car1; i++)
-        {
-
-        }
+        GameObject g = Instantiate(car1, transform.position, transform.rotation);
+        Destroy(g, 15);
+        spawnCheck = false;
     }
-    
+    IEnumerator SpawnTime()
+    {
+        yield return new WaitForSeconds(1);
+        spawnCheck = true;
+    }
     
 }
