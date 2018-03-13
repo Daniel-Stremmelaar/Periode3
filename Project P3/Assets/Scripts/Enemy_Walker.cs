f6cb35b1c7c124ccb6424336a2d1f6fdf18da12b
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-03-13 15:29:55 +0100

    change
    change level Pim

--- Project P3/Assets/Scripts/Enemy_Walker.cs
+++ Project P3/Assets/Scripts/Enemy_Walker.cs
@@ -7,15 +7,58 @@ public class Enemy_Walker : MonoBehaviour
 {
     private Transform player;
     private NavMeshAgent agent;
+    private float currentTime;
+    private bool senseFieldB;
+    [SerializeField] private float time;
 
-	void Start ()
+    [Header("Charging")]
+    [SerializeField] private Transform chargePoint;
+
+
+    void Start ()
     {
+        senseFieldB = false;
+        currentTime = time;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = gameObject.GetComponent<NavMeshAgent>();
 	}
+    void Update()
+    {
+        ThinkTimer();
+    }
 
-	void Update ()
+    void ThinkTimer()
+    {
+        if (senseFieldB)
+        {
+            currentTime -= Time.deltaTime;
+            agent.SetDestination(player.position);
+        }
+
+        if (currentTime <= 0)
+        {
+            currentTime = time;
+            senseFieldB = false;
+            agent.SetDestination(chargePoint.position);
+        }
+    }
+
+    void Sensefield()
     {
         agent.SetDestination(player.position);
-	}
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Sensefield();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        senseFieldB = true;
+    }
+
 }
