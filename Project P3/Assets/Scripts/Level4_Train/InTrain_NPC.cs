3e943c914bda19798c4f110bf5c1c042507806e7
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-03-20 08:46:28 +0100

    Update_01
    add models
    changed script

---
+++ Project P3/Assets/Scripts/Level4_Train/InTrain_NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTrain_NPC : MonoBehaviour
+{
+    public List<InTrain_NPCintfo> nPC = new List<InTrain_NPCintfo>();
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Project P3/Assets/Scripts/Level4_Train/InTrain_NPCintfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class InTrain_NPCintfo
+    
+{
+    public string question;
+    public List<string> answer = new List<string>();
+}
