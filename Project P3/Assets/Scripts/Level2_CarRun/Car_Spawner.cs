f2159becde31db1e4e92967c1516621fdc668495
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-03-22 14:02:44 +0100

    22-3
    tussenpuch

--- Project P3/Assets/Scripts/Level2_CarRun/Car_Spawner.cs
+++ Project P3/Assets/Scripts/Level2_CarRun/Car_Spawner.cs
@@ -2,35 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car_Spawner : MonoBehaviour
+public class Car_Spawner : Spawner_Base
 {
-    public GameObject car1;
-    public bool spawnCheck = true;
-    public float lifeTime;
-    public float spawn;
-
-    private void Update()
+    public override void Spawner()
     {
-        MaySpawn();
+        base.Spawner();
     }
-    public void MaySpawn()
-    {
-        if(spawnCheck == true)
-        {
-            StartCoroutine(SpawnTime());
-            Spawner();
-        }
-    }
-    public void Spawner()
-    {
-        GameObject g = Instantiate(car1, transform.position, transform.rotation);
-        Destroy(g, lifeTime);
-        spawnCheck = false;
-    }
-    IEnumerator SpawnTime()
-    {
-        yield return new WaitForSeconds(spawn);
-        spawnCheck = true;
-    }
-    
 }
---
+++ Project P3/Assets/Scripts/Level2_CarRun/Spawner_Base.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_Base : MonoBehaviour
+{
+    public List<GameObject> cars = new List<GameObject>();
+    private int random;
+    public bool spawnCheck = true;
+    public float lifeTime;
+    public float spawn;
+
+    void Start()
+    {
+        random = Random.Range(0,(cars.Count));
+    }
+    void Update()
+    {
+        MaySpawn();
+    }
+    public void MaySpawn()
+    {
+        if (spawnCheck == true)
+        {
+            StartCoroutine(SpawnTime());
+            Spawner();
+        }
+    }
+    public virtual void Spawner()
+    {
+        GameObject g = Instantiate(cars[random], transform.position, transform.rotation);
+        Destroy(g, lifeTime);
+        spawnCheck = false;
+    }
+    public virtual IEnumerator SpawnTime()
+    {
+        yield return new WaitForSeconds(spawn);
+        spawnCheck = true;
+    }
+
+}
