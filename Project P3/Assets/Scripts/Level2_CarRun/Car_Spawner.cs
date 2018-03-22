0ebf32d5f72bf2294004416d3a5d0dda46a3e78a
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-03-22 15:30:41 +0100

    22-3
    Laatste puch

--- Project P3/Assets/Scripts/Level2_CarRun/Car_Spawner.cs
+++ Project P3/Assets/Scripts/Level2_CarRun/Car_Spawner.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Car_Spawner : Spawner_Base
 {
+    private int car1;
+
+    void Start()
+    {
+        car1 = Random.Range(0, 1);
+    }
     public override void Spawner()
     {
         base.Spawner();
+        Instantiate(cars[car1], transform.position, transform.rotation);
     }
 }
