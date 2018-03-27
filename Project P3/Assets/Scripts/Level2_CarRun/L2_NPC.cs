e3b01315b7c3fc19659dd8a12e38cae125ba49b8
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-03-27 15:43:10 +0200

    27-3
    wat geprogameert

---
+++ Project P3/Assets/Scripts/Level2_CarRun/L2_NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2_NPC : MonoBehaviour
+{
+    public L2_Conversation conversation;
+    public void TriggerConversation()
+    {
+        FindObjectOfType<L2_ConversationManager>().StartConversation(conversation);
+    }
+	
+}
