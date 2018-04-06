df215a56b7f690ccfd267b3940bd613df9ef7996
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-04-06 15:16:55 +0200

    6-4
    beetje code

--- Project P3/Assets/Scripts/Level2_CarRun/L2_ConversationManager.cs
+++ Project P3/Assets/Scripts/Level2_CarRun/L2_ConversationManager.cs
@@ -7,29 +7,48 @@ public class L2_ConversationManager : MonoBehaviour
     public L2_Conversation conversation;
     private List<string> dialogueHolder = new List<string>();
 
+    public bool nextCenteces = false;
+
     void Update()
     {
         EndConversation();
         NextSentence();
+        
     }
 
     public void StartConversation(L2_Conversation conversation)
     {
         Debug.Log("StartConverstation");
-        
+        Debug.Log(conversation.dialogue[0]);
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("tab werkt");
+            nextCenteces = true;
+        }
     }
+    
     public void NextSentence()
     {
+        if (nextCenteces == true)
+        {
+            Debug.Log(conversation.dialogue[1]);
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Debug.Log(conversation.dialogue[2]);
+                nextCenteces = false;
+            }
+        }
+        
         //Left
         if (Input.GetKey ("1"))
         {
-            Debug.Log(conversation.dialogue[4]);
+            Debug.Log(conversation.dialogue[3]);
             
         }
         //Right
         if(Input.GetKey ("2"))
         {
-            Debug.Log(conversation.dialogue[5]);
+            Debug.Log(conversation.dialogue[4]);
         }
     }
     public void EndConversation()
