7101df820b05b16ca95ed2f8d462a2a7ec8cfe57
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-04-21 14:31:43 +0200

    21-4
    tussenpuch

---
+++ Project P3/Assets/Scripts/Level2_CarRun/L2_manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class L2_manager : MonoBehaviour
+{
+    public Button RestartButton;
+    public GameObject restartButtonSwitch;
+    public Vector3 death;
+
+    
+    void Start()
+    {
+        restartButtonSwitch.SetActive(false);
+        death.y = -2;
+    }
+
+    void Update()
+    {
+        GameOver();
+    }
+
+    public void GameOver()
+    {
+        if (GameObject.FindWithTag("Player").transform.position.y < -2)
+        {
+            GameObject.FindWithTag("Player").GetComponent<Rigidbody>().useGravity = false;
+            restartButtonSwitch.SetActive(true);
+            RestartButton.onClick.AddListener(Restart);
+        }
+    }
+
+    public void Restart()
+    {
+        if (restartButtonSwitch == true)
+        {
+            SceneManager.LoadScene("Jorrit");
+        }
+    }
+}
