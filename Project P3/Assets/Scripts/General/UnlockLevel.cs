974fa17d863dc3b60aff706b4a55b440a9717d5d
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-04-22 16:22:39 +0200

    add
    change
    texture

---
+++ Project P3/Assets/Scripts/General/UnlockLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UnlockLevel : MonoBehaviour
+{
+    [SerializeField] private string loadLevel;
+    public bool nextLevel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(loadLevel);
+            nextLevel = true;
+        }
+    }
+}
